25f6111de44f123a3659982060aed72c90032681
Kayo Werter <kayo.werter1@gmail.com>
2023-06-28 14:56:02 -0300

    atualizacoes para deploy

--- comunidade/__init__.py
+++ comunidade/__init__.py
@@ -2,13 +2,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 ''' O arquivo init é responsável pela importação e criação do nosso app e do banco de dados'''
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '17350bba99e89383d1ea7fe6c4cb5134'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+    
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
