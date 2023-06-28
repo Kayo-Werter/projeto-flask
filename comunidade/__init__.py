a56d8314d031911a40bb5a87031a781c0cd0dd5f
Kayo Werter <kayo.werter1@gmail.com>
2023-06-28 15:14:45 -0300

    Update __init__.py

--- comunidade/__init__.py
+++ comunidade/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 ''' O arquivo init é responsável pela importação e criação do nosso app e do banco de dados'''
 
@@ -21,6 +22,22 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+
+from comunidade import models
+
+
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspect = sqlalchemy.inspect(engine)
+
+if not inspect.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print('base de dados criada!')
+
+else:
+    print('Base de dados já existente!')
 
 '''A importação dos routes deve ser feita após a criação do app pelo fato de as routes precisarem do app pra funcionar
 '''
